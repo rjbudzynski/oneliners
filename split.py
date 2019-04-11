3446ca1fc93559ab75faac1b55ede9634d625ab0
rjbudzynski <39314526+rjbudzynski@users.noreply.github.com>
2019-04-11 15:50:27 +0200

    corrected data dir

--- split.py
+++ split.py
@@ -5,14 +5,14 @@ import sys, os, shutil
 try:
     arg = sys.argv[1]
 except IndexError:
-    exit(f'{sys.argv[0]}: file name required.')
-if os.path.exists('oneliners'):
-    shutil.rmtree('oneliners')
+    exit(f'{sys.argv[0]}: data file name required.')
+if os.path.exists('data'):
+    shutil.rmtree('data')
 os.mkdir('oneliners')
 
 with open(arg) as lines:
     for k, line in enumerate(lines):
         with open(f'oneliners/{k}', 'w') as f:
             f.write(line.strip())
-with open(f'oneliners/len', 'w') as f:
+with open(f'data/len', 'w') as f:
     f.write(str(k + 1))
