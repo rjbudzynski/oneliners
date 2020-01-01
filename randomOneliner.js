0b022b86214473c7eb9d750f75c5b70a8c2fb73d
Robert J Budzyński <robert.budzynski@fuw.edu.pl>
2020-01-01 14:50:58 +0100

    slight cleanup

--- randomOneliner.js
+++ randomOneliner.js
@@ -1,18 +1,29 @@
 #! /usr/bin/env node
+'use strict';
 
-"use strict";
 const fetch = require('node-fetch');
-const urlBase = "https://raw.githubusercontent.com/rjbudzynski/oneliners/master/data/";
+const urlBase =
+    'https://raw.githubusercontent.com/rjbudzynski/oneliners/master/data/';
 
-function getResponseBody(response) {
+const getResponseBody = response => {
     if (!response.ok) {
-        throw new Error(`${response.url}: ${response.status} ${response.statusText}`);
+        throw new Error(
+            `${response.url}: ${response.status} ${response.statusText}`
+        );
     }
-    return response.text()
-}
+    return response.text();
+};
 
-(async function printRandomOneliner() {
-    const len = await fetch(urlBase + "len").then(getResponseBody).then(parseInt);
+const printRandomOneliner = async () => {
+    const len = await fetch(urlBase + 'len')
+        .then(getResponseBody)
+        .then(parseInt);
     const index = Math.floor(len * Math.random());
-    fetch(urlBase + index).then(getResponseBody).then(console.log);
-})();
+    await fetch(urlBase + index)
+        .then(getResponseBody)
+        .then(console.log);
+};
+
+printRandomOneliner().catch(e => {
+    console.error(e);
+});
